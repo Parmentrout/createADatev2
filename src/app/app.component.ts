6e9a1a7ab6dd9ce5266b2d1d01bf598cba207754
Patrick Armentrout <patrick.armentrout@gmail.com>
2018-01-01 14:39:32 -0700

    Add authentication for google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { firestore } from 'firebase/app';
+
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  user: any;
+  test: any;
+  items: any;
+  constructor(private afAuth: AngularFireAuth) { //, private db: AngularFireDatabase
+    this.user = afAuth.authState;
+   // this.items = db.list('items');
+  }
+  login() {
+    
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+     this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,23 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { UiModule } from './ui/ui.module';
+import { ContentModule } from './content/content.module';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { AUTH_PROVIDERS} from 'angularfire2/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDg0mncwgtGjAOpYruM_yHq-Eb2pPWFNxw",
+  authDomain: "createadate-26879.firebaseapp.com",
+  databaseURL: "https://createadate-26879.firebaseio.com",
+  projectId: "createadate-26879",
+  storageBucket: "createadate-26879.appspot.com",
+  messagingSenderId: "851098752751"
+};
 
 @NgModule({
   declarations: [
@@ -15,6 +32,9 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
